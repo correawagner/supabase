297feb1c1d6d55d75a25a5d4f94df0cf8563f1fe
RampantDespair <rampantdespair@gmail.com>
2025-03-08 17:32:32 -0500

    Update generate-cert.sh

--- docker/generate-cert.sh
+++ docker/generate-cert.sh
@@ -10,8 +10,9 @@ KEY_FILE="$CERTS_DIR/key.pem"
 
 # Generate the self-signed certificate
 openssl req -x509 -newkey rsa:4096 -keyout "$KEY_FILE" -out "$CERT_FILE" -days 365 -nodes \
--subj "//CN=supabase-mail" \
--addext "subjectAltName = IP:172.19.0.2"
+-set_serial 01 \
+-addext "subjectAltName = DNS:supabase-mail" \
+-subj "/C=US/ST=State/L=City/O=Company/OU=Department/CN=supabase-mail"
 
 # Output result
 if [ $? -eq 0 ]; then
